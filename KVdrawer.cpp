7f8c2b0943006912efa3be5946c1eed35143a38f
Dejan <dejan@meznarc.si>
2020-05-29 23:03:41 +0200

    idk

--- KVdrawer.cpp
+++ KVdrawer.cpp
@@ -28,52 +28,56 @@ void KVdrawer::drawKVThree() {
 
 void KVdrawer::drawKVTwo() {
 
-    std::cout << " ====A====_______" << std::endl;
-    std::cout << "|   "               "    |" << "|   "               "    |" << std::endl;
-    std::cout << "|   " << outputs[2] << "   |" << "|   " << outputs[0] << "   |" << std::endl;
-    std::cout << "|   "               "    |" << "|   "               "    |" << std::endl;
-    std::cout << "|________________|" << std::endl;
-    std::cout << " _______________" << std::endl;
-    std::cout << "|   "               "    |" << "|   "               "   ||" << std::endl;
-    std::cout << "|   " << outputs[3] << "   |" << "|   " << outputs[1] << "  |B" << std::endl;
-    std::cout << "|   "               "    |" << "|   "               "   ||" << std::endl;
-    std::cout << "|_______________||" << std::endl;
+
+    std::cout << "                A " << std::endl;
+    std::cout << "       -----------------" << std::endl;
+    std::cout << "     |-------------------------------------------|" << std::endl;
+    std::cout << "   | |          |          |          |          |" << std::endl;
+    std::cout << " B | |     " << outputs[6] << "     |     " << outputs[7] << "     |      " << outputs[3] << "    |      " << outputs[2] << "    |" << std::endl;
+    std::cout << "   | |          |          |          |          |" << std::endl;
+    std::cout << "     |==========|==========|==========|==========|" << std::endl;
+    std::cout << "     |          |          |          |          |" << std::endl;
+    std::cout << "     |     " << outputs[4] << "     |     " << outputs[5] << "     |      " << outputs[1] << "    |      " << outputs[0] << "    |" << std::endl;
+    std::cout << "     |          |          |          |          |" << std::endl;
+    std::cout << "     |-------------------------------------------|" << std::endl;
+    std::cout << "                  ------------------" << std::endl;
+    std::cout << "                           C " << std::endl;
 }
 
-void KVdrawer::drawKVFour(char *sqValue) { //TODO bugg at sqValue[16]
+void KVdrawer::drawKVFour() {
 
     std::cout << " ========A========_________________" << std::endl;
     std::cout << "||  "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "    |" << std::endl;
-    std::cout << "||  " << sqValue[13] << "   |" << "|   " << sqValue[15] << "   |" << "|   " << sqValue[7]
+    std::cout << "||  " << outputs[13] << "   |" << "|   " << outputs[15] << "   |" << "|   " << outputs[7]
               << "   |"
-              << "|   " << sqValue[4] << "   |" << std::endl;
+              << "|   " << outputs[4] << "   |" << std::endl;
     std::cout << "||  "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "    |" << std::endl;
     std::cout << "||_________________________________|" << std::endl;
     std::cout << "|B_________________________________" << std::endl;
     std::cout << "||  "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "   ||" << std::endl;
-    std::cout << "||  " << sqValue[14] << "   |" << "|   " << sqValue[16] << "   |" << "|   " << sqValue[8]
+    std::cout << "||  " << outputs[14] << "   |" << "|   " << outputs[16] << "   |" << "|   " << outputs[8]
               << "   |"
-              << "|   " << sqValue[5] << "  ||" << std::endl;
+              << "|   " << outputs[5] << "  ||" << std::endl;
     std::cout << "||  "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "   ||" << std::endl;
     std::cout << "||________________________________||" << std::endl;
     std::cout << " _________________________________D|" << std::endl;
     std::cout << "|   "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "   ||" << std::endl;
-    std::cout << "|   " << sqValue[10] << "   |" << "|   " << sqValue[12] << "   |" << "|   " << sqValue[3]
+    std::cout << "|   " << outputs[10] << "   |" << "|   " << outputs[12] << "   |" << "|   " << outputs[3]
               << "   |"
-              << "|   " << sqValue[1] << "  ||" << std::endl;
+              << "|   " << outputs[1] << "  ||" << std::endl;
     std::cout << "|   "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "   ||" << std::endl;
     std::cout << "|_________________________________||" << std::endl;
     std::cout << " __________________________________" << std::endl;
     std::cout << "|   "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "    |" << std::endl;
-    std::cout << "|   " << sqValue[9] << "   |" << "|   " << sqValue[11] << "   |" << "|   " << sqValue[2] << "   |"
-              << "|   " << sqValue[0] << "   |" << std::endl;
+    std::cout << "|   " << outputs[9] << "   |" << "|   " << outputs[11] << "   |" << "|   " << outputs[2] << "   |"
+              << "|   " << outputs[0] << "   |" << std::endl;
     std::cout << "|   "               "    |" << "|   "               "    |" << "|   "               "    |"
               << "|   "               "    |" << std::endl;
     std::cout << "|________========C=========________|" << std::endl;
